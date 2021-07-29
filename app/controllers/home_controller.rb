12cdfb2e3a6f4047b19a8d049b048706fbd908bc
Ahmet Mirzooglu <ahmetmirzooglu@gmail.com>
2021-07-28 20:07:22 -0400

    installed devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+    if params[:ticker] == ""
+      @nothing = "Hey you forget to enter company name"
+    elsif params[:ticker]
+      @api = StockQuote::Stock.new(api_key: 'pk_02b698fe755e42279cee1fd909170b51')
+      @stock = StockQuote::Stock.quote(params[:ticker])
+      if !@stock
+        @error = "Hey that symbol does not exist"
+      end
+    end
+  end
+  def about
   end
 end
